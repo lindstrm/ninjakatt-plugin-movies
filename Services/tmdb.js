a5883c2fccb85b8f64b09c60cc7b6d0e990ddacc
Joachim Lindström <hello@jocke.fyi>
2021-01-04 09:14:56 +0100

    get all pages for upcoming

--- Services/tmdb.js
+++ Services/tmdb.js
@@ -3,11 +3,23 @@ const axios = require('axios').default;
 module.exports = class TMDB {
   constructor(apikey) {
     this.apikey = apikey;
+    this.tmp = [];
   }
 
   async getUpcoming(page = 1) {
     const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${this.apikey}&language=en-US&page=${page}&region=US`;
     const response = await axios.get(url);
+
+    if (response.data.total_pages > page) {
+      this.tmp = response.data.results;
+      return this.getUpcoming(page + 1);
+    }
+
+    if (this.tmp.length) {
+      response.data.results = [...response.data.results, ...this.tmp]
+      this.tmp = [];
+    }
+
     return response.data;
   }
 
